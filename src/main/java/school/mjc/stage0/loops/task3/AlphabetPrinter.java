8517e1fad68531a9ba4ccc80f966454a94ae8af5
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-16 18:05:45 -0400

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+                int a = 97, z = 122;
+        for (int t = a; t<=z; t++) {
+            char newLetter = (char) t;
+            System.out.println(newLetter);
+
+        }
     }
 }
