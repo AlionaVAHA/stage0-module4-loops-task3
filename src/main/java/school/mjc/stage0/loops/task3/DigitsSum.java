800461e7eabf2d64a7154f1bc875b9fda4d82e84
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-17 23:33:59 -0400

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,7 +2,6 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-           int number = 77;
         String newNumber = "" + number;
         int fres = 0;
         for (int t = 0; t < newNumber.length(); t++) {
