3cf402f0b1eb4d16de36a42d133dfce69161e028
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-17 23:32:44 -0400

    Update FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+         int number = 0;
+        int secondNumber = 1;
+        int number3 = 0;
+        System.out.println(number);
+        for (int t = 0; t < lastFibonacci; t++) {
+            if (number3 == lastFibonacci) {
+                System.out.println(number3);
+                break;
+            } else {
+                number3 = number + secondNumber;
+                number = secondNumber;
+                secondNumber = number3;
+                System.out.println(number);
+
+            }
+        }
     }
 }
