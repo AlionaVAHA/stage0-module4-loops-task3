d5a63d08cae761df5132cd899f26d056eb7ab7e3
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-17 22:58:10 -0400

    Update NinesSum.java

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
+        int result = 0;
+        int number = 9;
+        int neW = 9;
+        for (int t = 0; t < lengthOfLastNumber; t++) {
+            if (result == 0) {
+                result = number;
+            } else {
+                neW = neW * 10;
+                number = number + neW;
+                result += (number);
+            }
+        }
+        System.out.println(result);
 
     }
 }
