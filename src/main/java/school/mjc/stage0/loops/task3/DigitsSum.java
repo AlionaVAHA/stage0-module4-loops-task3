5c2232ec226a6ecd03c17f605874d254f28b1a93
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-17 23:41:41 -0400

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
+         String newNumber = "" + t;
+        int fres = 0;
+        for (int k = 0; k < newNumber.length(); k++) {
+            int newN = t % 10;
+            fres = fres + newN;
+            t = t / 10;
+        }
+        System.out.println(fres);
 
     }
 }
